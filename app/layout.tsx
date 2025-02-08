305c718a5a9893a271c02f1afdc900569e1a2e7b
Hossam Okasha <hossamoka4a@gmail.com>
2025-02-08 07:58:36 +0200

    feat: add MUI and initial theme provider setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import { PropsWithChildren } from "react";
+import { AppRouterCacheProvider as MuiAppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./_theme";
 
 const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
   subsets: ["latin"],
   display: "swap",
   variable: "--font-roboto",
+  fallback: ["Helvetica", "sans-serif"],
 });
 
 export const metadata: Metadata = {
@@ -17,7 +21,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
   return (
     <html lang="en">
-      <body className={roboto.variable}>{children}</body>
+      <MuiAppRouterCacheProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <body className={roboto.variable}>{children}</body>
+        </ThemeProvider>
+      </MuiAppRouterCacheProvider>
     </html>
   );
 }
