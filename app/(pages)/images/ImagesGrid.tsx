f75793ef8264e7af6d0122c31da66e82ac1e52a1
Hossam Okasha <hossamoka4a@gmail.com>
2025-02-11 07:08:18 +0200

    feat: filter images

--- app/(pages)/images/ImagesGrid.tsx
+++ app/(pages)/images/ImagesGrid.tsx
@@ -1,18 +1,26 @@
 "use client";
 
-import { Box } from "@mui/material";
+import {
+  Box,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField,
+} from "@mui/material";
 import AutoSizer from "react-virtualized-auto-sizer";
 import { VariableSizeGrid as Grid } from "react-window";
 import ImageItem from "./ImageItem";
 
 import ImagePreviewDialog from "@/app/_components/shared/ImagePreviewDialog";
 import { imageQueries } from "@/app/_constants/queryFactories";
+import useCategoriesOptions from "@/app/_hooks/useCategoriesOptions";
+import { debounce } from "@/app/_utils/debounce";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import DeleteImageAlert from "./DeleteImageAlert";
-import UpdateImageDialog from "./UpdateImageDialog";
-import ImagesWrapper from "./ImagesWrapper";
 import ImageSkeleton from "./ImageSkeleton";
+import ImagesWrapper from "./ImagesWrapper";
+import UpdateImageDialog from "./UpdateImageDialog";
 
 const gutterSize = 12; // Space between images
 
@@ -25,6 +33,18 @@ const ImagesGrid = () => {
   const [editingImageId, setEditingImageId] = useState<number | null>(null);
   const [imageToDelete, setImageToDelete] = useState<IImage | undefined>(
     undefined,
+  );
+  const { categoriesOptions = [] } = useCategoriesOptions();
+
+  const [search, setSearch] = useState("");
+  const [categoryId, setCategoryId] = useState(-1);
+
+  const debouncedSetSearch = debounce((val: string) => setSearch(val), 400);
+
+  const filiteredImages = images.filter(
+    (image) =>
+      image.name.toLowerCase().includes(search.toLowerCase()) &&
+      (categoryId === -1 || image.categoryId === categoryId),
   );
 
   const showEditImageDialog = !!editingImageId;
@@ -56,12 +76,12 @@ const ImagesGrid = () => {
     numColumns: number,
   ) => {
     const startIndex = rowIndex * numColumns;
-    const endIndex = Math.min(startIndex + numColumns, images.length);
+    const endIndex = Math.min(startIndex + numColumns, filiteredImages.length);
 
     let maxHeight = 200; // Default height
 
     for (let i = startIndex; i < endIndex; i++) {
-      const image = images[i];
+      const image = filiteredImages[i];
       if (!image) continue;
       const [width, height] = image.metadata.resolution.split("x").map(Number);
       const aspectRatio = width / height;
@@ -82,71 +102,116 @@ const ImagesGrid = () => {
   }
 
   return (
-    <Box sx={{ width: "100%", height: "90vh" }}>
-      <AutoSizer>
-        {({ height: containerHeight, width: containerWidth }) => {
-          let numColumns = 1;
-
-          if (containerWidth > 1200) {
-            numColumns = 3;
-          } else if (containerWidth > 800) {
-            numColumns = 2;
-          }
-
-          const numRows = Math.ceil(images.length / numColumns);
-          const colWidth =
-            (containerWidth - gutterSize * (numColumns - 1)) / numColumns;
-
-          return (
-            <Grid
-              columnCount={numColumns}
-              rowCount={numRows}
-              rowHeight={(ind) => getRowHeight(ind, colWidth, numColumns)}
-              columnWidth={() => colWidth}
-              width={containerWidth}
-              height={containerHeight}
-              className="window-grid"
-            >
-              {({ columnIndex, rowIndex, style }) => {
-                const imageIndex = rowIndex * numColumns + columnIndex;
-                if (imageIndex >= images.length) return null;
-                const image = images[imageIndex];
-
-                return (
-                  <Box paddingX={`${gutterSize}px`} style={style}>
-                    <ImageItem
-                      image={image}
-                      colWidth={colWidth}
-                      onEdit={() => onEdit(image.id)}
-                      onDelete={() => onDeleteImage(image)}
-                      onImageView={() => onImageView(image)}
-                    />
-                  </Box>
-                );
-              }}
-            </Grid>
-          );
+    <>
+      <TextField
+        variant="outlined"
+        placeholder="Search..."
+        slotProps={{
+          input: {
+            sx: {
+              borderRadius: 2,
+              backgroundColor: "white",
+            },
+          },
         }}
-      </AutoSizer>
+        sx={{
+          mb: 2,
+          marginInlineEnd: 2,
+          width: "100%",
+          maxWidth: 350,
+          height: 70,
+        }}
+        onChange={(e) => debouncedSetSearch(e.target.value)}
+      />
 
-      {imageToView && (
-        <ImagePreviewDialog
-          open={openImagePreview}
-          onClose={closeImagePreview}
-          dialogTitle={imageToView.name}
-          imgSrc={imageToView.url}
-          imgAlt={imageToView.name}
-          imgResolution={imageToView.metadata.resolution}
-        />
-      )}
-      <DeleteImageAlert onClose={onDeleteImageCancel} image={imageToDelete} />
-      {showEditImageDialog && (
-        <UpdateImageDialog
-          onClose={() => setEditingImageId(null)}
-          imageId={editingImageId}
-        />
-      )}
-    </Box>
+      {/* add loading  */}
+      <Select
+        value={categoryId?.toString()}
+        onChange={(e: SelectChangeEvent) =>
+          setCategoryId(Number(e.target.value))
+        }
+        variant="outlined"
+        sx={{
+          minWidth: 150,
+        }}
+      >
+        <MenuItem value="-1">All Categories</MenuItem>
+        {categoriesOptions?.map((category) => {
+          return (
+            <MenuItem key={category.value} value={category.value}>
+              {category.label}
+            </MenuItem>
+          );
+        })}
+      </Select>
+
+      {/* TODO: use window scroll */}
+      <Box sx={{ width: "100%", height: "90vh" }}>
+        <AutoSizer>
+          {({ height: containerHeight, width: containerWidth }) => {
+            let numColumns = 1;
+
+            if (containerWidth > 1200) {
+              numColumns = 3;
+            } else if (containerWidth > 800) {
+              numColumns = 2;
+            }
+
+            const numRows = Math.ceil(filiteredImages.length / numColumns);
+            const colWidth =
+              (containerWidth - gutterSize * (numColumns - 1)) / numColumns;
+
+            return (
+              <Grid
+                columnCount={numColumns}
+                rowCount={numRows}
+                rowHeight={(ind) => getRowHeight(ind, colWidth, numColumns)}
+                columnWidth={() => colWidth}
+                width={containerWidth}
+                height={containerHeight}
+                className="window-grid"
+              >
+                {({ columnIndex, rowIndex, style }) => {
+                  const imageIndex = rowIndex * numColumns + columnIndex;
+                  if (imageIndex >= filiteredImages.length) return null;
+                  const image = filiteredImages[imageIndex];
+
+                  return (
+                    <Box paddingX={`${gutterSize}px`} style={style}>
+                      <ImageItem
+                        image={image}
+                        colWidth={colWidth}
+                        onEdit={() => onEdit(image.id)}
+                        onDelete={() => onDeleteImage(image)}
+                        onImageView={() => onImageView(image)}
+                      />
+                    </Box>
+                  );
+                }}
+              </Grid>
+            );
+          }}
+        </AutoSizer>
+
+        {imageToView && (
+          <ImagePreviewDialog
+            open={openImagePreview}
+            onClose={closeImagePreview}
+            dialogTitle={imageToView.name}
+            imgSrc={imageToView.url}
+            imgAlt={imageToView.name}
+            imgResolution={imageToView.metadata.resolution}
+          />
+        )}
+        <DeleteImageAlert onClose={onDeleteImageCancel} image={imageToDelete} />
+        {showEditImageDialog && (
+          <UpdateImageDialog
+            onClose={() => setEditingImageId(null)}
+            imageId={editingImageId}
+          />
+        )}
+      </Box>
+    </>
   );
 };
 
